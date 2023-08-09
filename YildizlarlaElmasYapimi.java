f2fe10a3675d8485ec57cc3418bf9f3901b3dbe0
khasank <khasank@hotmail.com>
2023-08-09 05:59:08 +0300

    Create YildizlarlaElmasYapimi.java

---
+++ YildizlarlaElmasYapimi.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class YildizlarlaElmasYapimi {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
